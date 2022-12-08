c5f726223d511ffebb8bf5d22c6618b45ce03153
ghadaghalleb <ghadaghalleb07@gmail.com>
2022-12-08 02:05:39 +0100

    backend update

---
+++ backend/models/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  saltSecret: String
+});
+
+
+// Custom validation for email
+userSchema.path('email').validate((val) => {
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
+
+
+// Events
+userSchema.pre('save', function (next) {
+  bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(this.password, salt, (err, hash) => {
+          this.password = hash;
+          this.saltSecret = salt;
+          next();
+      });
+  });
+});
+
+ mongoose.model("user", UserSchema);
+
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
+require('./config/config');
+require('./models/db');
 
-const auth = require("./routes/api/auth");
-const users = require("./routes/api/users");
-const config = require("config");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const rtsIndex = require('./routes/index.router');
 
-const app = express();
+var app = express();
 
-app.use(express.json());
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', rtsIndex);
 
-const mongo_url = config.get("mongo_url");
+// error handler
+app.use((err, req, res, next) => {
+    if (err.name === 'ValidationError') {
+        var valErrors = [];
+        Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
+        res.status(422).send(valErrors)
+    }
+});
 
-mongoose
-  .connect(mongo_url, { useNewUrlParser: true , useUnifiedTopology: true , useCreateIndex: true })
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
-
-app.use("/api/auth", auth);
-app.use("/api/users", users);
-
-
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// start server
+app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
